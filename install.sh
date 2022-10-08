c6706c38a1cbb9b221646bfc1395fa1c4f18bdef
jpwakugawa <jp_wakugawa@outlook.com>
2022-10-08 19:38:21 -0400

    create nvim config folder and copy init.lua

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
-# Creates the configuration file
-touch ~/.config/nvim/init.vim
+NVIM_FOLDER="~/.config/nvim"
+
+# Check if nvim folder exists
+[ ! -d "$NVIM_FOLDER" ] && mkdir ~/.config/nvim
+
+# Copy the configuration file
+cp ./init.lua ~/.config/nvim/init.lua
