0f8aa6e7c8e27e9ad83249ebd6a43e16905b0f14
jpwakugawa <jp_wakugawa@outlook.com>
2022-11-03 16:57:03 -0400

    fix nvim configuration

--- install.sh
+++ install.sh
@@ -1,7 +1,20 @@
-NVIM_FOLDER="~/.config/nvim"
+NVIM_FOLDER=~/.config/nvim
 
-# Check if nvim folder exists
-[ ! -d "$NVIM_FOLDER" ] && mkdir ~/.config/nvim
+configure_nvim(){
+	# Check if nvim folder exists
+	if [ ! -d $NVIM_FOLDER ] 
+	then
+		mkdir -p "$NVIM_FOLDER"
+	fi
 
-# Copy the configuration file
-cp ./init.lua ~/.config/nvim/init.lua
+	# Copy the configuration file
+	cp init.lua ~/.config/nvim/
+}
+
+configure_nvim
+if [ $? -eq 0 ]
+then
+	echo "Configuration finished!"
+else
+	echo "Error during configuration..." 
+fi
