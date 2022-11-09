9a2e4806bdb79eeacdbd95ff38bdbd33296c023b
jpwakugawa <jp_wakugawa@outlook.com>
2022-11-08 20:21:21 -0400

    add indentation && search config to vimrc

--- init.lua
+++ init.lua
@@ -1,2 +1,14 @@
 -- Activate Relative Number
 vim.o.relativenumber = true
+
+-- Indentation
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
+
+-- Search Config
+vim.o.hlsearch = false
+vim.o.incsearch = true
+
