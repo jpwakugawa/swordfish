aa92b4764bbc9f73731404a634117fd9408fe102
jpwakugawa <jp_wakugawa@outlook.com>
2023-02-14 19:49:30 -0400

    add rose-pine colorsheme and transparent bg

--- custom/nvim/after/plugin/color.lua
+++ custom/nvim/after/plugin/color.lua
@@ -1,6 +1,10 @@
-vim.g.tokyonight_transparent_sidebar = true
-vim.g.tokyonight_transparent = true
-vim.opt.background = "dark"
+function ColorMyPencils(color)
+    color = color or "rose-pine"
+    vim.cmd.colorscheme(color)
 
-vim.cmd("colorscheme tokyonight")
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+ColorMyPencils()
 
--- custom/nvim/lua/config/packer.lua
+++ custom/nvim/lua/config/packer.lua
@@ -6,6 +6,20 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-  use 'folke/tokyonight.nvim'
+
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use({
+      'rose-pine/neovim',
+      as = 'rose-pine',
+      config = function()
+          require("rose-pine").setup()
+          vim.cmd('colorscheme rose-pine')
+      end
+  })
 end)
 
